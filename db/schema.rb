539efa5b6f3a56c589eca8ddbb3d49985242ab95
Josh Newth <joshnewth@yahoo.com>
2017-09-12 17:38:11 +0000

    Fix links and add index styling.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170821170146) do
+ActiveRecord::Schema.define(version: 20170911181413) do
 
   create_table "images", force: :cascade do |t|
     t.string   "name"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20170821170146) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
